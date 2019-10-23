761bc057e9a7966bc9b0a2d3498385c7675b1ef4
Victor Hermes <me.victorhermes@gmail.com>
2019-10-23 13:30:10 -0300

    profile

--- src/componentes/Content.js
+++ src/componentes/Content.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../css/Content.css'
 import Img from '../imgs/conteudo/01.jpg';
+import Perfil from '../imgs/profile.jpg';
 
 export default class Content extends React.Component {
     componentDidMount() {}
@@ -9,7 +10,8 @@ export default class Content extends React.Component {
         return (
             <section>
                 <div className="Cabecalho">
-                    <p>Perfil</p>
+                    <img src={Perfil} alt="Perfil" className="Perfil" />
+                    <p>Victor Hermes</p>
                 </div>
                 <img src={Img} alt="Natureza" className="Imagem" />
             </section>
