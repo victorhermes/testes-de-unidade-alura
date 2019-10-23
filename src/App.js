3875dd73fafb0a73b3f6f6d5201dea950712b804
Victor Hermes <me.victorhermes@gmail.com>
2019-10-23 09:32:36 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './componentes/Header';
+import './css/App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
