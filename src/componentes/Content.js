ba58aeef918a5597a5b961207e8b429f83a16959
Victor Hermes <me.victorhermes@gmail.com>
2019-10-23 13:37:12 -0300

    profile

--- src/componentes/Content.js
+++ src/componentes/Content.js
@@ -1,20 +1,22 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
 import '../css/Content.css'
 import Img from '../imgs/conteudo/01.jpg';
 import Perfil from '../imgs/profile.jpg';
 
-export default class Content extends React.Component {
+export default class Content extends Component {
     componentDidMount() {}
 
     render() {
         return (
-            <section>
-                <div className="Cabecalho">
-                    <img src={Perfil} alt="Perfil" className="Perfil" />
-                    <p>Victor Hermes</p>
-                </div>
-                <img src={Img} alt="Natureza" className="Imagem" />
-            </section>
+            <Fragment>
+                <section>
+                    <div className="Cabecalho">
+                        <img src={Perfil} alt="Perfil" className="Perfil" />
+                        <p>Victor Hermes</p>
+                    </div>
+                    <img src={Img} alt="Natureza" className="Imagem" />
+                </section>
+            </Fragment>
         )
     }
 }
