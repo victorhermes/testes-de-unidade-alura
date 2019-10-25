07b443cb9c8e1aad92631fd940cb4fbe468c43c1
Victor <me.victorhermes@gmail.com>
2019-10-25 13:16:33 -0300

    changes

--- src/componentes/Produto.js
+++ src/componentes/Produto.js
@@ -3,59 +3,59 @@ import Menu from "./Menu";
 import "../css/Produto.css";
 
 export default class Produto extends Component {
-    state = {
-        moeda: "R$"
-    };
+        state = {
+            moeda: "R$"
+        };
 
-    subtrairLances = (num1, num2) => {
-        return num1 - num2;
-    };
+        subtrairLances = (num1, num2) => {
+            return num1 - num2;
+        };
 
-    render() {
-        const { id, titulo, imagem, lances } = this.props.location.dados;
+        render() {
+            const { id, titulo, imagem, lances } = this.props.location.dados;
 
-        const maioresLances = lances
-            .sort(this.subtrairLances)
-            .reverse()
-            .slice(0, 3);
+            const maioresLances = lances
+                .sort(this.subtrairLances)
+                .reverse()
+                .slice(0, 3);
 
-        return (
-            <Fragment>
-                <Menu />
-                <section key={id} className="produto">
-                    <div>
-                        <img
-                            src={imagem}
-                            alt={titulo}
-                            className="imagem-produto"
-                        />
-                    </div>
-                    <div className="conteudo">
-                        <h1 className="titulo-produto">{titulo}</h1>
-                        <h3>Maior lance:</h3>
-                        <p className="valor-maior">
-                            {this.state.moeda}
-                            {Math.max(...lances)}
-                        </p>
+            return (
+                <Fragment>
+                    <Menu />
+                    <section key={id} className="produto">
+                        <div>
+                            <img
+                                src={imagem}
+                                alt={titulo}
+                                className="imagem-produto"
+                            />
+                        </div>
+                        <div className="conteudo">
+                            <h1 className="titulo-produto">{titulo}</h1>
+                            <h3>Maior lance:</h3>
+                            <p className="valor-maior">
+                                {this.state.moeda}
+                                {Math.max(...lances)}
+                            </p>
 
-                        <h3>Menor lance:</h3>
-                        <p className="valor-menor">
-                            {this.state.moeda}
-                            {Math.min(...lances)}
-                        </p>
+                            <h3>Menor lance:</h3>
+                            <p className="valor-menor">
+                                {this.state.moeda}
+                                {Math.min(...lances)}
+                            </p>
 
-                        <h3>Maiores lances:</h3>
-                        {maioresLances.map(lance => {
-                            return (
-                                <p key={lance} className="valor-neutro">
-                                    {this.state.moeda}
-                                    {lance}
-                                </p>
-                            );
-                        })}
-                    </div>
-                </section>
-            </Fragment>
-        );
-    }
+                            <h3>Maiores lances:</h3>
+                            {maioresLances.map(lance => {
+                                return (
+                                    <p key={lance} className="valor-neutro">
+                                        {this.state.moeda}
+                                        {lance}
+                                    </p>
+                                );
+                            })}
+                        </div>
+                    </section>
+                </Fragment>
+            );
+        }
 }
