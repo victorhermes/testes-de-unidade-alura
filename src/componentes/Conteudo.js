a032f4b38fbf6991d43cce796a992470f87957a8
Victor <me.victorhermes@gmail.com>
2019-10-25 13:17:11 -0300

    changes

--- src/componentes/Conteudo.js
+++ src/componentes/Conteudo.js
@@ -5,56 +5,56 @@ import Menu from "./Menu";
 import "../css/Conteudo.css";
 
 export default class Conteudo extends Component {
-    state = {
-        moeda: "R$",
-        dados: []
-    };
+        state = {
+            moeda: "R$",
+            dados: []
+        };
 
-    componentDidMount() {
-        fetch(URLBase)
-            .then(response => response.json())
-            .then(data => {
-                this.setState({ dados: data });
-            })
-            .catch(error => console.error(error));
-    }
+        componentDidMount() {
+            fetch(URLBase)
+                .then(response => response.json())
+                .then(data => {
+                    this.setState({ dados: data });
+                })
+                .catch(error => console.error(error));
+        }
 
-    render() {
-        return (
-            <Fragment>
-                <Menu />
-                {this.state.dados.map(conteudo => {
-                    return (
-                        <section className="container" key={conteudo.id}>
-                            <div>
-                                <img
-                                    src={conteudo.imagem}
-                                    alt={conteudo.titulo}
-                                    className="imagem"
-                                />
-                            </div>
-                            <div className="conteudo">
-                                <h1 className="titulo-conteudo">
-                                    {conteudo.titulo}
-                                </h1>
-                                <p className="valor">
-                                    {this.state.moeda}
-                                    {Math.max(...conteudo.lances)}
-                                </p>
-                                <Link
-                                    to={{
-                                        pathname: `/produto/${conteudo.id}`,
-                                        dados: conteudo
-                                    }}
-                                    className="botao-conferir"
-                                >
-                                    Conferir
-                                </Link>
-                            </div>
-                        </section>
-                    );
-                })}
-            </Fragment>
-        );
-    }
+        render() {
+            return (
+                <Fragment>
+                    <Menu />
+                    {this.state.dados.map(conteudo => {
+                        return (
+                            <section className="container" key={conteudo.id}>
+                                <div>
+                                    <img
+                                        src={conteudo.imagem}
+                                        alt={conteudo.titulo}
+                                        className="imagem"
+                                    />
+                                </div>
+                                <div className="conteudo">
+                                    <h1 className="titulo-conteudo">
+                                        {conteudo.titulo}
+                                    </h1>
+                                    <p className="valor">
+                                        {this.state.moeda}
+                                        {Math.max(...conteudo.lances)}
+                                    </p>
+                                    <Link
+                                        to={{
+                                            pathname: `/produto/${conteudo.id}`,
+                                            dados: conteudo
+                                        }}
+                                        className="botao-conferir"
+                                    >
+                                        Conferir
+                                    </Link>
+                                </div>
+                            </section>
+                        );
+                    })}
+                </Fragment>
+            );
+        }
 }
