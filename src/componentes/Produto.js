9003c93411060d64dd83f0b5c1b5384c054b0bf8
Victor <me.victorhermes@gmail.com>
2019-10-29 20:53:22 -0300

    refatoração do componente produto

--- src/componentes/Produto.js
+++ src/componentes/Produto.js
@@ -1,60 +1,77 @@
 import React, { Component, Fragment } from "react";
+import { URLBase } from "../services/api";
 import Menu from "./Menu";
 import "../css/Produto.css";
 
 export default class Produto extends Component {
     state = {
-        moeda: "R$"
+        moeda: "R$",
+        dados: []
     };
+
+    componentDidMount() {
+        const { id } = this.props.match.params;
+        fetch(URLBase + "/" + id)
+            .then(response => response.json())
+            .then(data => {
+                this.setState({ dados: [data] });
+            })
+            .catch(error => console.error(error));
+    }
 
     subtrairLances = (num1, num2) => {
         return num1 - num2;
     };
 
     render() {
-        const { id, titulo, imagem, lances } = this.props.location.dados;
-
-        const maioresLances = lances
-            .sort(this.subtrairLances)
-            .reverse()
-            .slice(0, 3);
-
         return (
             <Fragment>
                 <Menu />
-                <section key={id} className="produto">
-                    <div>
-                        <img
-                            src={imagem}
-                            alt={titulo}
-                            className="imagem-produto"
-                        />
-                    </div>
-                    <div className="conteudo">
-                        <h1 className="titulo-produto">{titulo}</h1>
-                        <h3>Maior lance:</h3>
-                        <p className="valor-maior">
-                            {this.state.moeda}
-                            {Math.max(...lances)}
-                        </p>
-
-                        <h3>Menor lance:</h3>
-                        <p className="valor-menor">
-                            {this.state.moeda}
-                            {Math.min(...lances)}
-                        </p>
-
-                        <h3>Maiores lances:</h3>
-                        {maioresLances.map(lance => {
-                            return (
-                                <p key={lance} className="valor-neutro">
+                {this.state.dados.map(conteudo => {
+                    return (
+                        <section key={conteudo.id} className="produto">
+                            <div>
+                                <img
+                                    src={conteudo.imagem}
+                                    alt={conteudo.titulo}
+                                    className="imagem-produto"
+                                />
+                            </div>
+                            <div className="conteudo">
+                                <h1 className="titulo-produto">
+                                    {conteudo.titulo}
+                                </h1>
+                                <h3>Maior lance:</h3>
+                                <p className="valor-maior">
                                     {this.state.moeda}
-                                    {lance}
+                                    {Math.max(...conteudo.lances)}
                                 </p>
-                            );
-                        })}
-                    </div>
-                </section>
+                                <h3>Menor lance:</h3>
+                                <p className="valor-menor">
+                                    {this.state.moeda}
+                                    {Math.min(...conteudo.lances)}
+                                </p>
+
+                                <h3>Maiores lances:</h3>
+                                {conteudo.lances
+                                    .sort(this.subtrairLances)
+                                    .reverse()
+                                    .slice(0, 3)
+                                    .map(lance => {
+                                        return (
+                                            <p
+                                                key={lance}
+                                                className="valor-neutro"
+                                            >
+                                                {this.state.moeda}
+                                                {lance}
+                                            </p>
+                                        );
+                                    })}
+                            </div>
+                        </section>
+                    );
+                })}
             </Fragment>
         );
     }
