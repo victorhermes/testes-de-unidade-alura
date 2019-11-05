c5950480fbb8b76cc77fd3f36630a3f6bef41e13
Victor <me.victorhermes@gmail.com>
2019-11-04 21:49:50 -0300

    testes

--- src/__tests__/components/Menu.spec.js
+++ src/__tests__/components/Menu.spec.js
@@ -7,8 +7,6 @@ describe("Testes do menu", () => {
     it("Deve renderizar o menu", () => {
         const wrapper = shallow(<Menu />);
 
-        //console.log(wrapper.debug());
-
         expect(wrapper.find("Link")).toHaveLength(2);
         expect(wrapper.find(".link-produto").text()).toEqual(
             "Adicionar Produto"
